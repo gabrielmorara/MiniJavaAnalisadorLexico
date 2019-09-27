a99034e09f0be14d2c080c958cfe8c0bb1d6ed91
Gabriel Morara <gabriel.morara@megamamute.com.br>
2019-09-27 14:18:01 -0300

    Versao final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Coloque o caminho do arquivo de teste no parametro abaixo
-        System.out.println(ServiceFile.readFileAndWriteOutput("C:\\Users\\PC\\Desktop\\GM\\faculdade\\Compiladores\\src\\test\\java\\Example"));
+        System.out.println(ServiceFile.readFileAndWriteOutput("C:\\Users\\PC\\Desktop\\GM\\faculdade\\Compiladores\\src\\test\\java\\Factorial.java"));
     }
 }
 
